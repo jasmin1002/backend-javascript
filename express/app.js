1d131bd50b32630e88462ace4baf956470b6cdad
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-12 23:28:27 +0100

    refactor: Restructure code with MVC model

--- express/app.js
+++ express/app.js
@@ -1,20 +1,10 @@
 const express = require('express')
-const data = require('./data')
-console.log(data);
+const router = require('./router/routes')
 const app = express()
 
 // app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.json(data)
-})
-
-app.post("/api/people", (req, res) => {
-    console.log(req.body)
-    // data = [...data, req.body]
-    res.status(201).send([...data, req.body]);
-})
+app.use(router);
 
 app.listen(8000, () => {
     console.log('Server is listening on port 6000')
