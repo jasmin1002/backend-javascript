801759ebc3549f83d8bec341d4dc04d1cf519eb2
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-12 22:19:05 +0100

    feat: Add basic endpoints for routing

--- express/app.js
+++ express/app.js
@@ -1,1 +1,21 @@
-console.log('I am learning express...');
+const express = require('express')
+const data = require('./data')
+console.log(data);
+const app = express()
+
+// app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json(data)
+})
+
+app.post("/api/people", (req, res) => {
+    console.log(req.body)
+    // data = [...data, req.body]
+    res.status(201).send([...data, req.body]);
+})
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 6000')
+})
